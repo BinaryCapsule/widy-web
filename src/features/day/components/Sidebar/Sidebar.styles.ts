3479c6445752beef5bd5f5066e22f1b6f37041af
Jose Nunes <josenunes34@gmail.com>
2021-09-04 12:41:50 +0100

    fix: sidebar padding

--- src/features/day/components/Sidebar/Sidebar.styles.ts
+++ src/features/day/components/Sidebar/Sidebar.styles.ts
@@ -4,7 +4,7 @@ export const SidebarWrapper = styled.aside`
   min-width: 360px;
   flex-grow: 1;
   background: ${({ theme }) => theme.colors.yellow['50']};
-  padding: 48px 32px;
+  padding: 38px 32px;
 `;
 
 export const SidebarContent = styled.div`
