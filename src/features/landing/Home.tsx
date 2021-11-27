972e2fdcbac194202c3202e46ccc8dbe7d773e12
Jose Nunes <jose.nunes@orion.com>
2021-11-27 12:06:22 +0000

    fix: build

--- src/features/landing/Home.tsx
+++ src/features/landing/Home.tsx
@@ -22,56 +22,59 @@ const Home: React.FC = () => {
     }
   });
 
-  // @ts-ignore
   return (
-    <Flex height="100%" maxWidth={800} flexDirection="column" mx="auto" px="32">
-      <Flex as="header" justifyContent="flex-end" py="24">
-        <Button
-          variant="ghost"
-          variantColor="neutral"
-          leftIcon="login"
-          iconVariant="outline"
-          onClick={loginWithRedirect}
-        >
-          Log in
-        </Button>
-      </Flex>
-
-      <Box as="main" flex={1}>
-        <Flex alignItems="flex-end">
-          <IconWidy size={isWide ? 90 : 60} yesterdayColor={theme.colors.blue['600']} mr="8" />
-          <IconWidyText size={isWide ? 180 : 120} textColor={theme.colors.blue['600']} />
+    <>
+      {/* @ts-ignore */}
+      <Flex height="100%" maxWidth={800} flexDirection="column" mx="auto" px="32">
+        <Flex as="header" justifyContent="flex-end" py="24">
+          <Button
+            variant="ghost"
+            variantColor="neutral"
+            leftIcon="login"
+            iconVariant="outline"
+            onClick={loginWithRedirect}
+          >
+            Log in
+          </Button>
         </Flex>
 
-        <Text fontSize={isWide ? 'h2' : 'h4'} fontWeight={500} mb="8">
-          What I did Yesterday?
-        </Text>
+        <Box as="main" flex={1}>
+          <Flex alignItems="flex-end">
+            <IconWidy size={isWide ? 90 : 60} yesterdayColor={theme.colors.blue['600']} mr="8" />
+            <IconWidyText size={isWide ? 180 : 120} textColor={theme.colors.blue['600']} />
+          </Flex>
 
-        <Box maxWidth={500}>
-          <Text fontWeight={500}>
-            Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            Nulla vitae elit libero, a pharetra augue.
+          <Text fontSize={isWide ? 'h2' : 'h4'} fontWeight={500} mb="8">
+            What I did Yesterday?
           </Text>
+
+          <Box maxWidth={500}>
+            <Text fontWeight={500}>
+              Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+              Nulla vitae elit libero, a pharetra augue.
+            </Text>
+          </Box>
+
+          <Box ml={isWide ? -250 : -100} mt={isWide ? -45 : 12}>
+            <HeroIllustration width={isWide ? 800 : 500} />
+          </Box>
         </Box>
 
-        <Box ml={isWide ? -250 : -100} mt={isWide ? -45 : 12}>
-          <HeroIllustration width={isWide ? 800 : 500} />
-        </Box>
-      </Box>
-
-      <Flex as="footer" justifyContent="flex-end" py="24">
-        <IconButton
-          as="a"
-          href="https://github.com/BinaryCapsule/widy-web"
-          variant="ghost"
-          variantColor="neutral"
-          size="medium"
-          icon="github"
-          iconVariant="solid"
-          aria-label="GitHub link"
-        />
+        <Flex as="footer" justifyContent="flex-end" py="24">
+          <IconButton
+            as="a"
+            // @ts-ignore
+            href="https://github.com/BinaryCapsule/widy-web"
+            variant="ghost"
+            variantColor="neutral"
+            size="medium"
+            icon="github"
+            iconVariant="solid"
+            aria-label="GitHub link"
+          />
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   );
 };
 
