736ea24ae2805075e9008567be1b3d6b8511c86f
Jose Nunes <josenunes@hey.com>
2022-01-04 00:15:52 +0000

    fix: tasks table tasks per scope

--- src/features/report/components/DayReport/components/TasksTable/useTasksTable.ts
+++ src/features/report/components/DayReport/components/TasksTable/useTasksTable.ts
@@ -1,54 +1,55 @@
 import { DayReportDto, ReportTaskDto } from '../../../../api/useDayReportQuery';
 import { useMemo } from 'react';
 
+interface TasksPerScope {
+  scopeTitle: string;
+  id: number;
+  time: number;
+  tasks: Pick<ReportTaskDto, 'id' | 'summary' | 'isDone' | 'time'>[];
+}
+
 export const selectTasksTableData = ({ tasks }: DayReportDto) => {
-  const tasksPerScope = tasks.reduce(
-    (acc, task) => {
-      if (task.time === 0) {
-        return acc;
-      }
+  const noScope: TasksPerScope = {
+    scopeTitle: 'No Scope',
+    id: -1,
+    tasks: [],
+    time: 0,
+  };
 
-      const { scope } = task;
-
-      if (!scope) {
-        acc[0].tasks.push(task);
-      } else {
-        const scopeIndex = acc.findIndex(({ id: scopeId }) => scopeId === scope.id);
-
-        if (scopeIndex === -1) {
-          acc.push({
-            id: scope.id,
-            scopeTitle: `${scope.name} - ${scope.shortCode.toUpperCase()}`,
-            tasks: [task],
-          });
-        } else {
-          acc[scopeIndex].tasks.push(task);
-        }
-      }
-
+  const tasksPerScope = tasks.reduce((acc, task) => {
+    if (task.time === 0) {
       return acc;
-    },
-    [
-      {
-        scopeTitle: 'No Scope',
-        id: -1,
-        tasks: [],
-        time: 0,
-      },
-    ] as {
-      scopeTitle: string;
-      id: number;
-      time?: number;
-      tasks: Pick<ReportTaskDto, 'id' | 'summary' | 'isDone' | 'time'>[];
-    }[],
-  );
+    }
 
-  // Sum all tasks time and add it as the scope's time property
-  return tasksPerScope.reduce((acc, scope, index) => {
-    // eslint-disable-next-line no-param-reassign
-    acc[index].time = scope.tasks.reduce((totalTime, { time }) => totalTime + time, 0);
+    const { scope } = task;
+
+    if (!scope) {
+      noScope.tasks.push(task);
+      noScope.time += task.time;
+    } else {
+      const scopeIndex = acc.findIndex(({ id: scopeId }) => scopeId === task.scopeId);
+
+      if (scopeIndex === -1) {
+        acc.push({
+          id: scope.id,
+          scopeTitle: `${scope.name} - ${scope.shortCode.toUpperCase()}`,
+          tasks: [task],
+          time: task.time,
+        });
+      } else {
+        acc[scopeIndex].tasks.push(task);
+        acc[scopeIndex].time += task.time;
+      }
+    }
+
     return acc;
-  }, tasksPerScope);
+  }, [] as TasksPerScope[]);
+
+  if (noScope.time > 0) {
+    tasksPerScope.push(noScope);
+  }
+
+  return tasksPerScope;
 };
 
 interface UseTasksTableParams {
